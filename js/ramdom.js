6fe6f9ffd42aa832b7cba17ae6d4a738305b5a5c
mickael anicette <mickaelanicette@gmail.com>
2021-11-19 17:42:39 -0300

    rajout css et correction bug

--- js/ramdom.js
+++ js/ramdom.js
@@ -9,7 +9,6 @@ function deleteAllF() {
 function submitAllF(event) {
     let chaineForm = document.getElementById("multiple-textarea").value
     localStorage.setItem('arrayElement',chaineForm)
-    afficherElement()
     afficherSelection()
     // For this example, don't actually submit the form
     event.preventDefault();
@@ -18,51 +17,40 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 async function afficherSelection() {
-    let arrayElements = document.getElementsByClassName("mini")
     let equipeInput = document.querySelector('input[name="equipe-radio"]:checked').value
     if (equipeInput=="0") {
+        afficherElement()
+        let arrayElements = document.getElementsByClassName("mini")
         for (let i = 0; i < arrayElements.length; i++) {
             await new Promise(r => setTimeout(r, 150));
             if (i==0) {
-                arrayElements[i].style.background = "red"
+                arrayElements[i].style.background = "#b7b7a4"
             }else{
-                arrayElements[i-1].style.background = "cornsilk"
-                arrayElements[i].style.background = "red"
+                arrayElements[i-1].style.background = "transparent"
+                arrayElements[i].style.background = "#b7b7a4"
             } 
         }
         await new Promise(r => setTimeout(r, 150));
-        arrayElements[arrayElements.length-1].style.background = "cornsilk"
-        arrayElements[getRandomInt(arrayElements.length-1)].style.background = "blue"
+        arrayElements[arrayElements.length-1].style.background = "transparent"
+        arrayElements[getRandomInt(arrayElements.length-1)].style.background = "#b5e48c"
     }else{
+        document.getElementById('droite-section').innerHTML = ""
         let nombreEquipeInput = document.getElementById("nombre-equipe-input").value
         let liste = melangerArray(localStorage.getItem('arrayElement').split(','))
         let listeSliced = sliceArray(liste,nombreEquipeInput)
-        document.getElementById('droite-section').innerHTML = ""
         for (let i = 0; i < listeSliced.length; i++) {
             const miniListe = listeSliced[i];
             p = document.createElement("p")
             p.setAttribute("class","mini")
             p.innerHTML = "Equipe n°"+(i+1)
             document.getElementById('droite-section').append(p)
-            alert(miniListe)
             miniListe.forEach(element => {
-                alert("aaaa")
                 pa = document.createElement("p")
                 pa.setAttribute("class","mini")
                 pa.innerHTML = element
                 document.getElementById('droite-section').append(pa) 
             }); 
             
-        }
-        for (const miniArray of listeSliced) {
-            alert(miniArray)
-
-/*             miniArray.forEach(element => {
-                pa = document.createElement("p")
-                pa.setAttribute("class","mini")
-                pa.innerHTML = element
-                document.getElementById('droite-section').append(p) 
-            }); */
         }
     }
     
@@ -72,6 +60,7 @@ function melangerArray(arr) {
     return newArray
 }
 function sliceArray(array,nombre) {
+    nombre = parseInt(nombre)
     const slicedArray = []
     for (let i = 0; i < array.length; i+=nombre) {
         const miniArray = array.slice(i,(i+nombre));
@@ -81,6 +70,7 @@ function sliceArray(array,nombre) {
 }
 function afficherElement() {
     if (localStorage.getItem('arrayElement')!=null){
+        document.getElementById('droite-section').innerHTML = ""
         const arrayChaine = localStorage.getItem('arrayElement').split(',')
         arrayChaine.forEach(element => {
             p = document.createElement("p")
